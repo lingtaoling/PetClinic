a9f136e7a96f1bf1f94e2381aa4d8a39a25d58a4
KrakenZim <104575615+KrakenZim@users.noreply.github.com>
2022-10-13 10:14:46 -0400

    First Push
    First Push not 100% about the file structure... if you hit any bugs maybe we can add or delete some stuff

---
+++ repo/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VetClinic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAppointments_Click(object sender, RoutedEventArgs e)
+        {
+           Home.Content = new Appointments();
+        }
+        private void BtnPetProfile_Click(object sender, RoutedEventArgs e)
+        {
+            Home.Content = new PetProfile();
+        }
+
+        private void BtnVets_Click(object sender, RoutedEventArgs e)
+        {
+            Home.Content = new Vets();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Home.Content = new Login();
+        }
+
+        private void BtnPrescriptions_Click(object sender, RoutedEventArgs e)
+        {
+            Home.Content = new Prescriptions();
+        }
+
+        private void BtnBookAppointment_Click(object sender, RoutedEventArgs e)
+        {
+            Home.Content = new BookAppointment();
+        }
+    }
+}
